588d024efe1a2e029b7d21ad3206c2e41b2664f7
Chris <chrisdedeian@gmail.com>
2018-08-10 13:38:30 -0700

    Initial commit

---
+++ CalculatorTester.java
@@ -0,0 +1,10 @@
+public class CalculatorTester {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        // calc.setOperandOne(10.5);
+        // calc.setOperandTwo(5.2);
+        // calc.setOperator("-");
+        // calc.getResults();
+        System.out.println(calc.getOperandOne());
+    }
+}
